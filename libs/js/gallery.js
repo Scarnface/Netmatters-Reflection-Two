4fbd7603f1f9add480c9c4cc9e98251b3a9842ef
Scarnface <scarnface@tutanota.com>
2021-07-30 13:38:59 +0100

    Anaimated menu button added. Partials broken down further.

--- libs/js/gallery.js
+++ libs/js/gallery.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
+  //Load the slick carousel and apply the required options.
   $(".carousel").slick({
     autoplay: true,
     autoplaySpeed: 4000,
     arrows: false,
     dots: true,
   });
-});
 
-$(".services-gallery").mousedown(function () {
-  $(".services-gallery").css("cursor", "grab");
-});
+  // Switch between the grab and default styling on mouse down/up.
+  $(".services-gallery").mousedown(function () {
+    $(".services-gallery").css("cursor", "grab");
+  });
 
-$(".services-gallery").mouseup(function () {
-  $(".services-gallery").css("cursor", "default");
+  $(".services-gallery").mouseup(function () {
+    $(".services-gallery").css("cursor", "default");
+  });
 });
---
+++ libs/js/menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // Toggle the main menu button between bars and cross styling.
+  $("#nav-icon").click(function () {
+    $(this).toggleClass("open");
+  });
+});
