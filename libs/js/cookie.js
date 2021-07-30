2b31cceacf66c01068d416b27c7c4d65d2c61d82
Scarnface <scarnface@tutanota.com>
2021-07-30 10:49:27 +0100

    Cookie modal completed

---
+++ libs/js/cookie.js
@@ -0,0 +1,17 @@
+if (localStorage.getItem("cookieSeen") != "shown") {
+  // Calculate width of screen minus scroll bar width.
+  var scrollBarWidth = window.innerWidth - document.body.offsetWidth;
+  // Disable scrolling on page while modal open and adjust page for scroll bar.
+  $("body").css({ marginRight: scrollBarWidth, overflow: "hidden" });
+  // Display the cookie modal.
+  $(".cookie-banner").css("display", "flex");
+}
+
+$(".cookieClose").click(function () {
+  // Save the cookie acceptance to local storage.
+  localStorage.setItem("cookieSeen", "shown");
+  // Reset the scroll bar and page adjustment.
+  $("body").css({ "margin-right": "", overflow: "visible" });
+  // Hide the cookie modal.
+  $(".cookie-banner").css("display", "none");
+});
