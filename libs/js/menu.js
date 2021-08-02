42bac5399e9618c9c17cc75971c2f5a11375132c
Scarnface <scarnface@tutanota.com>
2021-08-02 15:06:08 +0100

    Modal menu functionality complete. First menu layout added.

--- libs/js/menu.js
+++ libs/js/menu.js
@@ -1,6 +1,29 @@
 $(document).ready(function () {
-  // Toggle the main menu button between bars and cross styling.
+  // The element that scolling persists for.
+  const targetElement = document.querySelector(".menu-wrapper");
+
   $("#nav-icon").click(function () {
-    $(this).toggleClass("open");
+    // Toggle the main menu button between bars and cross styling.
+    $("#nav-icon").toggleClass("open");
+    // Toggle the modal menu
+    $(".menu-banner").toggleClass("open");
+    // Adjust main page width for menu.
+    $(".main").toggleClass("open");
+    // Disable body scroll.
+    bodyScrollLock.disableBodyScroll(targetElement);
+  });
+
+  // Listens for clicks on the modal and checks to see if the event target is the menu element.
+  $(".menu-banner").click(function (event) {
+    if (!$(event.target).closest(".menu-wrapper").length) {
+      // Toggle the main menu button between bars and cross styling.
+      $("#nav-icon").toggleClass("open");
+      // Toggle the modal menu
+      $(".menu-banner").toggleClass("open");
+      // Adjust main page width for menu.
+      $(".main").toggleClass("open");
+      // Re-enable body scroll.
+      bodyScrollLock.enableBodyScroll(targetElement);
+    }
   });
 });
